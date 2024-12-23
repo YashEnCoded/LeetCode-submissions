e358bb5fa6ca79a893086763f4f451d853f7aadb
YashEnCoded <indianyashg@gmail.com>
2024-12-23 21:04:50 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (20.41%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,9 @@
+// Dec'23, 2024 09:04 pm
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+        return n*(n+1)/2- accumulate(nums.begin(), nums.end(), 0);
+    }
+};
