fb67e150a096efa7def2303fa039ac4c2afee1e3
YashEnCoded <indianyashg@gmail.com>
2025-01-06 19:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (24.96%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,19 +1,34 @@
 // POTD Jan'6, 2024
-// Jan'6, 2024 06:45 pm
+// Jan'6, 2024 07:10 pm
 
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n=boxes.size();
         vector<int>ans(n, 0);
+        vector<int>pre(n+1, 0);
+        vector<int>suf(n+1, 0);
+        int cnt=0;
+
+        cnt= boxes[0]-'0';
+        for(int i=1;i<n;i++){
+            pre[i]= pre[i-1] + cnt;
+            cnt+= boxes[i] - '0'; 
+        }
+
+        cnt= boxes[n-1] - '0';
+        for(int i=n-2;i>=0;i--){
+            suf[i]= suf[i+1] + cnt;
+            cnt+= boxes[i] - '0';
+        }
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans[i]+= (boxes[j]-'0')*(abs(j-i));
-            }
+            ans[i]= pre[i]+ suf[i];
         }
 
         return ans;
     }
 };
 
-// 4 min
+// 3 min
+// O(N) soln
